81d43b1ee3900a6fe540dc1f8af998aa547ad867
Rogif <135497831+Rogif@users.noreply.github.com>
2024-07-27 15:20:39 +0300

    Assets ready

--- sketch.js
+++ sketch.js
@@ -1,26 +1,25 @@
 let grad;
 let back;
 let wave;
-
-
+let divevideo;
 
 function setup() {
   createCanvas(windowWidth, 6493);
+  divevideo = createVideo(['divevideo.mp4']);
+  divevideo.loop(); // Loop the divevideo once when the program starts
 }
 
 function preload() {
-  grad = loadImage('seagradient.png');
-  back = loadImage('seaimage.png');
-  wave = loadImage('waveimage.png');
+  // No need to load the video here since it's already loaded in setup
 }
 
 function draw() {
   background(255); // Use a solid color background if needed, or remove this line if not
-  image(back, 0, 0, windowWidth, 1080); // Draw 'back' covering the entire canvas
-  image(grad, 0, 1050, windowWidth, height); // Draw 'grad' on top, covering the entire canvas
-  blendMode(LIGHTEST); // Set blend mode to lighten
-  tint(255, 200); // Apply transparency without changing color
-  image(wave, 0, 500, windowWidth, 885); // Draw 'wave' on top of 'back', covering the entire canvas
-  noTint(); // Remove transparency
-  blendMode(BLEND); // Reset blend mode to default
+  image(divevideo, 0, 0, windowWidth, 6493); // Display the divevideo
+
+  // Torch effect
+  let opacity = map(mouseY, 0, height, 0, 255); // Map mouseY to opacity values
+  fill(255, 255, 0, opacity); // Set fill color to yellow with dynamic opacity
+  noStroke(); // No border for the circle
+  ellipse(mouseX, mouseY, 273, 273); // Draw the yellow circle at the mouse position
 }
