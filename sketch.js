636489be1188e2627f00a7acb69664da1a11a7a1
Oren Partesana <orenpa@post.bgu.ac.il>
2024-07-27 16:41:16 +0300

    Fish and video in place

--- sketch.js
+++ sketch.js
@@ -2,24 +2,51 @@ let grad;
 let back;
 let wave;
 let divevideo;
+let playing = false;
+let numDepths = 5;
+let fish = ["turtle.gif", "octopus.gif", "angler.gif", "lantern fish.png", "Tubeworms.png"];
+let seaStartHeight = 1000;
+let fishScale = 0.3;
 
 function setup() {
   createCanvas(windowWidth, 6493);
-  divevideo = createVideo(['divevideo.mp4']);
-  divevideo.loop(); // Loop the divevideo once when the program starts
+  divevideo.hide();
+  divevideo.loop();
 }
 
 function preload() {
-  // No need to load the video here since it's already loaded in setup
+  divevideo = createVideo(['divevideo.mp4']);
+  fishImages = [];
+  for (let i = 0; i < fish.length; i++) {
+    fishImages.push(loadImage(fish[i]));
+  }
 }
 
 function draw() {
-  background(255); // Use a solid color background if needed, or remove this line if not
-  image(divevideo, 0, 0, windowWidth, 6493); // Display the divevideo
+  background(200); // Set the background color to black
+  image(divevideo, 0, 0, width, height); // Draw the divevideo at the top left corner of the canvas
+
+  for (let i = 0; i < numDepths; i++) {
+    let depth = map(i, 0, numDepths, seaStartHeight, height); // Map the depth to a y-coordinate
+    let fishIndex = i % fish.length; // Calculate the index of the fish to display
+    image(fishImages[fishIndex], 0, depth, width * fishScale, (height - seaStartHeight) * fishScale, 0, 0, fishImages[fishIndex].width, fishImages[fishIndex].height, CONTAIN); // Draw the fish at the current depth
+  }
+
 
   // Torch effect
   let opacity = map(mouseY, 0, height, 0, 255); // Map mouseY to opacity values
   fill(255, 255, 0, opacity); // Set fill color to yellow with dynamic opacity
   noStroke(); // No border for the circle
   ellipse(mouseX, mouseY, 273, 273); // Draw the yellow circle at the mouse position
+}
+
+function mousePressed() {
+  divevideo.loop(); // Loop the divevideo when the mouse is pressed
+}
+
+function mouseMoved() {
+  if (!playing) {
+    divevideo.loop();
+    playing = true;
+  }
 }
