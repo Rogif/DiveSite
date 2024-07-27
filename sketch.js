065b56a3274e25e2b50e671df2dc064a32c4f332
Oren Partesana <orenpa@post.bgu.ac.il>
2024-07-27 20:03:35 +0300

    Flashlight works

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function preload() {
   for (let i = 0; i < depthLabels.length; i++) {
     labelImages.push(loadImage("meter/" + depthLabels[i]));
   }
-  flashlight = loadImage("flashlight.png");
+  flashlight = loadImage("flashlightinverted.png");
   gradient = loadImage("blackgradient.png");
 }
 
@@ -40,28 +40,28 @@ function draw() {
     image(fishImages[fishIndex], width / 2, depth, width * fishScale, (height - seaStartHeight) * fishScale, 0, 0, fishImages[fishIndex].width, fishImages[fishIndex].height, CONTAIN); // Draw the fish at the current depth
   }
 
-  image(gradient, 0, seaStartHeight, width, height - seaStartHeight);
+  gradientcopy = createImage(gradient.width, gradient.height);
+  gradientcopy.copy(gradient, 0, 0, gradient.width, gradient.height, 0, 0, gradient.width, gradient.height);
+  // Torch effect
+  //let opacity = map(mouseY, 0, height, 0, 255); // Map mouseY to opacity values
+  //fill(255, 255, 0, opacity); // Set fill color to yellow with dynamic opacity
+  //noStroke(); // No border for the circle
+  //torchEllipse = ellipse(mouseX, mouseY, 273, 273); // Draw the yellow circle at the mouse position
+  if (mouseY > seaStartHeight) { // Check if the mouse is within the bounds of the flashlight
+    gradientcopy.blend(flashlight, 0, 0, flashlight.width, flashlight.height, (mouseX / width) * gradientcopy.width - 200, (mouseY / height) * (gradientcopy.height + seaStartHeight) - 200 - seaStartHeight, flashlight.width, flashlight.height, BLEND); // Blend the flashlight with the gradient
+  }
 
-  // Draw the gradient
+
+  blendMode(DARKEST);
+  image(gradientcopy, 0, seaStartHeight, width, height - seaStartHeight);
+  //blend(flashlight, 0, 0, flashlight.width, flashlight.height, mouseX - 136, mouseY - 136, 273, 273, MULTIPLY); // Blend the flashlight with
+  blendMode(BLEND); // Set the blend mode to normal
 
   for (let i = 0; i < numDepths; i++) {
     let depth = map(i, 0, numDepths, seaStartHeight, height); // Map the depth to a y-coordinate
     let fishIndex = i % fish.length; // Calculate the index of the fish to display
     image(labelImages[i], 0, depth, width * labelScale, (height - seaStartHeight) * labelScale, 0, 0, labelImages[i].width, labelImages[i].height, CONTAIN); // Draw the label at the
   }
-
-  // Torch effect
-  //let opacity = map(mouseY, 0, height, 0, 255); // Map mouseY to opacity values
-  //fill(255, 255, 0, opacity); // Set fill color to yellow with dynamic opacity
-  //noStroke(); // No border for the circle
-  //torchEllipse = ellipse(mouseX, mouseY, 273, 273); // Draw the yellow circle at the mouse position
-
-  blendMode(MULTIPLY); // Set the blend mode to REMOVE
-  image(flashlight, mouseX - 136, mouseY - 136, 273, 273, 0, 0, flashlight.width, flashlight.height); // Remove the flashlight from the canvas
-
-  blendMode(BLEND);
-  //blend(flashlight, 0, 0, flashlight.width, flashlight.height, mouseX - 136, mouseY - 136, 273, 273, MULTIPLY); // Blend the flashlight with
-
 }
 
 function mousePressed() {
